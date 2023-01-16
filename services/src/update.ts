24c66dad0e85732dd8e52d1c1176360f8337b383
Meeta Maity <meeta@studiographene.com>
2023-01-16 10:41:03 +0530

    Custom JWT Auth

--- services/src/update.ts
+++ services/src/update.ts
@@ -1,32 +1,45 @@
+import logger from "./../../utils/logger";
 import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  APIGatewayProxyHandlerV2WithJWTAuthorizer,
+  // APIGatewayProxyEvent,
+  // APIGatewayProxyResult,
+  APIGatewayProxyHandlerV2,
+  // APIGatewayProxyHandlerV2WithJWTAuthorizer,
 } from "aws-lambda";
-
+import jwt from "jsonwebtoken";
 import notes from "./notes";
 
-export const handler: APIGatewayProxyHandlerV2WithJWTAuthorizer =
-  async function main(event) {
-    const note =
-      event.pathParameters && event.pathParameters.id
-        ? notes[event.pathParameters.id]
-        : null;
-
-    if (!note) {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({ error: true }),
-      };
-    }
-
-    if (event.body) {
-      const data = JSON.parse(event.body);
-      note.content = data.content || note.content;
-    }
-
+export const handler: APIGatewayProxyHandlerV2 = async function main(event) {
+  let username: string;
+  const token: string = event.headers["authorization"]?.split(" ")[1] || "";
+  console.log(token);
+  if (token) {
+    console.log(jwt.verify(token, "secret"));
+  } else {
     return {
-      statusCode: 200,
-      body: JSON.stringify(note, null, " "),
+      statusCode: 404,
+      body: JSON.stringify({ error: true }),
     };
+  }
+
+  const note =
+    event.pathParameters && event.pathParameters.id
+      ? notes[event.pathParameters.id]
+      : null;
+
+  if (!note) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: true }),
+    };
+  }
+
+  if (event.body) {
+    const data = JSON.parse(event.body);
+    note.content = data.content || note.content;
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(note, null, " "),
   };
+};
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -5,9 +5,13 @@ export default class MyStack extends sst.Stack {
     super(scope, id, props);
 
     // Create User Pool
-    const auth = new sst.Cognito(this, "Auth", {
-      login: ["email"],
-    });
+    // const auth = new sst.Cognito(this, "Auth", {
+    //   login: ["email"],
+    // });
+
+    // const auth = new sst.Auth(this, "Auth", {
+    //   authenticator: "functions/authenticator.handler",
+    // });
 
     // Create the HTTP API
 
@@ -25,39 +29,49 @@ export default class MyStack extends sst.Stack {
       //   authorizer: "auth0",
       // },
 
-      authorizers: {
-        jwt: {
-          type: "user_pool",
-          userPool: {
-            id: auth.userPoolId,
-            clientIds: [auth.userPoolClientId],
-          },
-        },
-      },
-      defaults: {
-        authorizer: "jwt",
-      },
+      // authorizers: {
+      //   jwt: {
+      //     type: "user_pool",
+      //     userPool: {
+      //       id: auth.userPoolId,
+      //       clientIds: [auth.userPoolClientId],
+      //     },
+      //   },
+      // },
+      // defaults: {
+      //   authorizer: "jwt",
+      // },
       routes: {
-        "GET /notes": {
-          function: "src/list.handler",
-          authorizer: "none",
-        },
-        "GET /notes/{id}": {
-          function: "src/get.handler",
-          authorizer: "none",
-        },
+        "POST /signup": "src/signup.handler",
+        // {
+        //   function: "src/signup.handler",
+        //   authorizer: "none",
+        // },
+        "GET /notes": "src/list.handler",
+        // {
+        //   function: "src/list.handler",
+        //   authorizer: "none",
+        // },
+        "GET /notes/{id}": "src/get.handler",
+        // {
+        //   function: "src/get.handler",
+        //   authorizer: "none",
+        // },
         "PUT /notes/{id}": "src/update.handler",
       },
     });
 
+    // auth.attach(this, {
+    //   api,
+    // });
     // allowing authenticated users to access API
-    auth.attachPermissionsForAuthUsers(this, [api]);
+    // auth.attachPermissionsForAuthUsers(this, [api]);
 
     // Show the API endpoint and other info in the output
     this.addOutputs({
       ApiEndpoint: api.url,
-      UserPoolId: auth.userPoolId,
-      UserPoolClientId: auth.userPoolClientId,
+      // UserPoolId: auth.userPoolId,
+      // UserPoolClientId: auth.userPoolClientId,
     });
   }
 }
