8a82d76aa8f0a47b047330617e5e149c01bdc754
RuanGomsSG <ruan.gomes@studiographene.com>
2023-03-24 17:41:46 +0000

    feat: import handler as notes

--- services/notes/functions/list.ts
+++ services/notes/functions/list.ts
@@ -1,5 +1,5 @@
 import { APIGatewayProxyResult } from 'aws-lambda';
-import { handler as notes } from 'services/notes/functions/notes';
+import { handler as notes } from 'functions/notes';
 
 export const handler =
   async function handler(): Promise<APIGatewayProxyResult> {
