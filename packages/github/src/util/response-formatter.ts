f2c08271b30c584a53279684c6b24ac75c114c56
charchit khandelwal <charchit.khandelwal@studiographene.com>
2023-06-09 12:19:38 +0530

    feat(PX-406): github user details api
    Signed-off-by: charchit khandelwal <charchit.khandelwal@studiographene.com>

--- packages/github/src/util/response-formatter.ts
+++ packages/github/src/util/response-formatter.ts
@@ -1,9 +1,18 @@
 export const searchedDataFormator = async (data: any) => {
   if (data?.hits?.max_score != null) {
     return data.hits.hits.map((hit: any) => ({
-      id: hit._id,
+      _id: hit._id,
       ...hit._source.body,
     }));
   }
   return [];
+};
+export const formatUserDataResponse = (data: any) => {
+  return {
+    id: data._id,
+    githubId: data.id,
+    userName: data.userName,
+    avatarUrl: data.avatarUrl,
+    organizationId: data.organizationId,
+  };
 };
