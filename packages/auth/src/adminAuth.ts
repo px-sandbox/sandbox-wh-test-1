926e4a4ff66f50fb592547b91154b72e612e38f7
NarendraSG <narendra@studiographene.com>
2023-06-19 16:51:29 +0530

    feat(PX-376): feedback fix

--- packages/auth/src/adminAuth.ts
+++ packages/auth/src/adminAuth.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import { handler as auth } from './auth';
+import { Other } from 'abstraction';
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context) =>
-  auth(event, context, 'ADMIN');
+  auth(event, context, Other.Enum.Role.ADMIN);
