8fe877a230fdbd111ad408c22c815bc1efe88891
charchit khandelwal <charchit.khandelwal@studiographene.com>
2023-08-19 11:59:34 +0530

    fix(PX-000): rate limit check inside cron
    Signed-off-by: charchit khandelwal <charchit.khandelwal@studiographene.com>

--- packages/github/src/cron/retry-processes.ts
+++ packages/github/src/cron/retry-processes.ts
@@ -1,11 +1,23 @@
 import { DynamoDbDocClient } from '@pulse/dynamodb';
 import { SQSClient } from '@pulse/event-handler';
 import { logger } from 'core';
+import { ghRequest } from 'src/lib/request-defaults';
 import { RetryTableMapping } from 'src/model/retry-table-mapping';
+import { getInstallationAccessToken } from 'src/util/installation-access-token-generator';
 
 export async function handler() {
   logger.info(`RetryProcessHandler invoked at: ${new Date().toISOString()}`);
-
+  const installationAccessToken = await getInstallationAccessToken();
+  const octokit = ghRequest.request.defaults({
+    headers: {
+      Authorization: `Bearer ${installationAccessToken.body.token}`,
+    },
+  });
+  const githubRetryLimit = await octokit('GET /rate_limit');
+  if (githubRetryLimit.data && githubRetryLimit.data.rate.remaining === 0) {
+    logger.info('NO_REMANING_RATE_LIMIT', { githubRetryLimit: githubRetryLimit.data });
+    return false;
+  }
   const processes = await new DynamoDbDocClient().scan(
     new RetryTableMapping().prepareScanParams(1000)
   );
