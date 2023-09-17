b0d1208986cf914bfa45150706bfb87c33c2b306
Meeta Maity <meeta@studiographene.com>
2023-09-17 10:17:48 +0530

    feat(PX-7342): update schedule

--- stacks/github.ts
+++ stacks/github.ts
@@ -61,7 +61,7 @@ function intializeCron(
   if (stackStage === Stage.LIVE) {
     // eslint-disable-next-line no-new
     new Cron(stack, 'github-copilot-cron', {
-      schedule: 'cron(0/60 * ? * * *)',
+      schedule: 'cron(0 * ? * * *)',
       job: ghCopilotFunction,
     });
   }
