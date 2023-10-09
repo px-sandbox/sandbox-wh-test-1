18ca0fc0b100dd530fcd6679f66160b1bb42dd3a
charchit khandelwal <charchit.khandelwal@studiographene.com>
2023-10-09 15:46:26 +0530

    feat(PX-9000): refreshToken and accessToken to be upated in every 45 minute
    Signed-off-by: charchit khandelwal <charchit.khandelwal@studiographene.com>

--- packages/jira/src/cron/refresh-token.ts
+++ packages/jira/src/cron/refresh-token.ts
@@ -24,6 +24,7 @@ export async function updateRefreshToken(): Promise<void> {
   }
   await Promise.all([
     ddbResp.map(async (item) => {
+      logger.info(`Updating Refresh token updated for credId...: ${item.credId}`);
       const newRefreshToken = await getTokens(item.refreshToken);
       await _ddbClient.put(new JiraCredsMapping().preparePutParams(item.credId, newRefreshToken));
       logger.info(`Refresh token updated for credId: ${item.credId}`);
