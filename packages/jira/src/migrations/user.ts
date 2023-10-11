5eda9d6da8aa87c07889b1eb650674b430f944ef
Meeta Maity <meeta@studiographene.com>
2023-10-11 11:54:04 +0530

    feat(PX-7522): fix some lint issues

--- packages/jira/src/migrations/user.ts
+++ packages/jira/src/migrations/user.ts
@@ -4,11 +4,12 @@ import { SQSClient } from '@pulse/event-handler';
 import { Jira } from 'abstraction';
 import { Queue } from 'sst/node/queue';
 
-export const handler = async function (event: SQSEvent) {
-  const sqsClient = new SQSClient();
-  await Promise.all(
-    event.Records.map((record: SQSRecord) => {
-      try {
+export const handler = async function userMigration(event: SQSEvent): Promise<void> {
+  try {
+    const sqsClient = new SQSClient();
+    await Promise.all(
+      event.Records.map((record: SQSRecord) => {
+
         const { organization, user }: { organization: string; user: Jira.ExternalType.Api.User } =
           JSON.parse(record.body);
         const createdAt = new Date().toISOString();
@@ -23,9 +24,10 @@ export const handler = async function (event: SQSEvent) {
           },
           Queue.jira_user_format.queueUrl
         );
-      } catch (error) {
-        logger.error(JSON.stringify({ error, record }));
-      }
-    })
-  );
+
+      })
+    );
+  } catch (error) {
+    logger.error(JSON.stringify({ error, event }));
+  }
 };
