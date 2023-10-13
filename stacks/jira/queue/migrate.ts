294ff79e93f449eaa78f4826a2a3f4ba9a9b11dc
charchit khandelwal <charchit.khandelwal@studiographene.com>
2023-10-13 17:27:40 +0530

    feat(PX-871): migrate queues
    Signed-off-by: charchit khandelwal <charchit.khandelwal@studiographene.com>

--- stacks/jira/queue/migrate.ts
+++ stacks/jira/queue/migrate.ts
@@ -1,4 +1,4 @@
-import { Queue, use } from 'sst/constructs';
+import { Function, Queue, use } from 'sst/constructs';
 import { Duration, Stack } from 'aws-cdk-lib';
 import { commonConfig } from '../../common/config';
 import { JiraTables } from '../../type/tables';
@@ -15,101 +15,86 @@ export function initializeMigrateQueue(
     formatterQueues;
 
   const sprintMigrateQueue = new Queue(stack, 'jira_sprint_migrate', {
-    consumer: {
-      function: {
-        handler: 'packages/jira/src/migrations/sprint.handler',
-        timeout: '300 seconds',
-        runtime: 'nodejs18.x',
-        bind: [
-
-          sprintFormatter,
-          ...Object.values(jiraDDB),
-          ...Object.values(envs)
-        ],
-      },
-      cdk: {
-        eventSource: {
-          batchSize: 5,
-        },
-      },
-    },
     cdk: {
       queue: {
         visibilityTimeout: Duration.seconds(310),
       },
     }
+  });
+  sprintMigrateQueue.addConsumer(stack, {
+    function: new Function(stack, 'jira_sprint_migrate_func', {
+      handler: 'packages/jira/src/migrations/sprint.handler',
+      timeout: '300 seconds',
+      runtime: 'nodejs18.x',
+      bind: [
+        sprintMigrateQueue,
+        sprintFormatter,
+        ...Object.values(jiraDDB),
+        ...Object.values(envs)
+      ],
+    }),
+    cdk: {
+      eventSource: {
+        batchSize: 5,
+      },
+    },
   });
 
   const boardMigrateQueue = new Queue(stack, 'jira_board_migrate', {
-    consumer: {
-      function: {
-        handler: 'packages/jira/src/migrations/board.handler',
-        timeout: '300 seconds',
-        runtime: 'nodejs18.x',
-        bind: [
-          boardFormatter,
-          ...Object.values(jiraDDB),
-          ...Object.values(envs)
-        ],
-      },
-      cdk: {
-        eventSource: {
-          batchSize: 5,
-        },
-      },
-    },
     cdk: {
       queue: {
         visibilityTimeout: Duration.seconds(310),
       },
     }
+  });
+
+  boardMigrateQueue.addConsumer(stack, {
+    function: new Function(stack, 'jira_board_migrate_func', {
+      handler: 'packages/jira/src/migrations/board.handler',
+      timeout: '300 seconds',
+      runtime: 'nodejs18.x',
+      bind: [
+        boardMigrateQueue,
+        boardFormatter,
+        ...Object.values(jiraDDB),
+        ...Object.values(envs)
+      ],
+    }),
+    cdk: {
+      eventSource: {
+        batchSize: 5,
+      },
+    },
   });
 
   const projectMigrateQueue = new Queue(stack, 'jira_project_migrate', {
-    consumer: {
-      function: {
-        handler: 'packages/jira/src/migrations/project.handler',
-        timeout: '300 seconds',
-        runtime: 'nodejs18.x',
-        bind: [
-
-          projectFormatter,
-          ...Object.values(jiraDDB),
-          ...Object.values(envs)
-        ],
-      },
-      cdk: {
-        eventSource: {
-          batchSize: 5,
-        },
-      },
-    },
     cdk: {
       queue: {
         visibilityTimeout: Duration.seconds(310),
       },
     }
+  });
+
+  projectMigrateQueue.addConsumer(stack, {
+    function: new Function(stack, 'jira_project_migrate_func', {
+      handler: 'packages/jira/src/migrations/project.handler',
+      timeout: '300 seconds',
+      runtime: 'nodejs18.x',
+      bind: [
+        projectMigrateQueue,
+        projectFormatter,
+        ...Object.values(jiraDDB),
+        ...Object.values(envs)
+      ],
+    }),
+    cdk: {
+      eventSource: {
+        batchSize: 5,
+      },
+    },
   });
 
   const issueMigrateQueue = new Queue(stack, 'jira_issue_migrate', {
-    consumer: {
-      function: {
-        handler: 'packages/jira/src/migrations/issue.handler',
-        timeout: '300 seconds',
-        runtime: 'nodejs18.x',
-        bind: [
-
-          issueFormatter,
-          ...Object.values(jiraDDB),
-          ...Object.values(envs)
-        ],
-      },
-      cdk: {
-        eventSource: {
-          batchSize: 5,
-        },
-      },
-    },
     cdk: {
       queue: {
         visibilityTimeout: Duration.seconds(310),
@@ -117,30 +102,50 @@ export function initializeMigrateQueue(
     }
   });
 
-  const userMigrateQueue = new Queue(stack, 'jira_user_migrate', {
-    consumer: {
-      function: {
-        handler: 'packages/jira/src/migrations/user.handler',
-        timeout: '300 seconds',
-        runtime: 'nodejs18.x',
-        bind: [
-
-          userFormatter,
-          ...Object.values(jiraDDB),
-          ...Object.values(envs)
-        ],
-      },
-      cdk: {
-        eventSource: {
-          batchSize: 5,
-        },
+  issueMigrateQueue.addConsumer(stack, {
+    function: new Function(stack, 'jira_issue_migrate_func', {
+      handler: 'packages/jira/src/migrations/issue.handler',
+      timeout: '300 seconds',
+      runtime: 'nodejs18.x',
+      bind: [
+        issueMigrateQueue,
+        issueFormatter,
+        ...Object.values(jiraDDB),
+        ...Object.values(envs)
+      ],
+    }),
+    cdk: {
+      eventSource: {
+        batchSize: 5,
       },
     },
+  });
+
+  const userMigrateQueue = new Queue(stack, 'jira_user_migrate', {
     cdk: {
       queue: {
         visibilityTimeout: Duration.seconds(310),
       },
     }
+  });
+
+  userMigrateQueue.addConsumer(stack, {
+    function: new Function(stack, 'jira_user_migrate_func', {
+      handler: 'packages/jira/src/migrations/user.handler',
+      timeout: '300 seconds',
+      runtime: 'nodejs18.x',
+      bind: [
+        userMigrateQueue,
+        userFormatter,
+        ...Object.values(jiraDDB),
+        ...Object.values(envs)
+      ],
+    }),
+    cdk: {
+      eventSource: {
+        batchSize: 5,
+      },
+    },
   });
 
   projectMigrateQueue.bind([boardMigrateQueue]);
