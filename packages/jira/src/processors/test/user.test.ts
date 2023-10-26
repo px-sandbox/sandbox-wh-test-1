f20a0e2641acfb983e332782f92fa488bcaf7486
Meeta Maity <meeta@studiographene.com>
2023-10-26 15:36:54 +0530

    feat(PX-2929): handle test cases for user and board

--- packages/jira/src/processors/test/user.test.ts
+++ packages/jira/src/processors/test/user.test.ts
@@ -1,78 +1,138 @@
-import { beforeEach, expect, it, describe, vi, MockedFunction, MockedClass } from 'vitest';
+import { beforeEach, afterEach, expect, it, describe, vi } from 'vitest';
 import { Jira } from 'abstraction';
 import { logger } from 'core';
+import { JiraClient } from '../../lib/jira-client';
 import { UserProcessor } from '../user';
 
-const mockUser = {
-    id: '123',
-    body: {
-        id: '456',
-        userId: '789',
-        emailAddress: 'johndoe@example.com',
-        displayName: 'John Doe',
+// eslint-disable-next-line max-lines-per-function
+describe('UserProcessor', () => {
+    let jiraClient: JiraClient;
+    let userProcessor: UserProcessor;
+    let apiData: Jira.Mapper.User;
+    const user: Jira.Mapper.User = {
+        self: 'https://example.com/user123',
+        accountId: 'user123',
+        accountType: Jira.Enums.UserType.Atlassian,
+        emailAddress: 'user@example.com',
+        displayName: 'User Example',
         avatarUrls: {
-            avatarUrl48x48: 'https://example.com/avatars/johndoe.png',
-            avatarUrl32x32: 'https://example.com/avatars/johndoe-32.png',
-            avatarUrl24x24: 'https://example.com/avatars/johndoe-24.png',
-            avatarUrl16x16: 'https://example.com/avatars/johndoe-16.png',
+            '48x48': 'https://example.com/avatar48.png',
+            '32x32': 'https://example.com/avatar32.png',
+            '24x24': 'https://example.com/avatar24.png',
+            '16x16': 'https://example.com/avatar16.png',
         },
-        isActive: true,
+        active: true,
+        timeZone: 'America/New_York',
         isDeleted: false,
         deletedAt: null,
         createdAt: '2022-01-01T00:00:00.000Z',
-        organizationId: 'abc',
-    },
-};
-
-vi.mock('src/constant/config', () => ({
-    mappingPrefixes: 'jira_user',
-}));
-vi.mock('src/model/params-mapping', (mockParamsMapping) => ({
-    ParamsMapping: mockParamsMapping,
-}));
-
-// eslint-disable-next-line max-lines-per-function
-describe('userFormattedDataReciever', () => {
-    let mockUserProcessor = vi.spyOn(
-        UserProcessor.prototype, 'processor') as MockedFunction<typeof UserProcessor.prototype.processor>;
-    let mockUserValidate = vi.spyOn(
-        UserProcessor.prototype, 'validate') as MockedFunction<typeof UserProcessor.prototype.validate>;
-    // const mockUserProcessor = UserProcessor as MockedClass<typeof UserProcessor>;
-    const mockSendDataToQueue = vi.spyOn(
-        UserProcessor.prototype, 'sendDataToQueue') as MockedFunction<typeof UserProcessor.prototype.sendDataToQueue>;
-    // const mockSendDataToQueue = vi.fn();
-
+        organization: 'org 123',
+    };
     beforeEach(() => {
-        vi.clearAllMocks();
-        vi.useFakeTimers();
+        apiData = user;
+        userProcessor = new UserProcessor(apiData);
+        jiraClient = {
+            getUser: vi.fn(),
+        } as unknown as JiraClient;
+        JiraClient.getClient = vi.fn().mockResolvedValue(jiraClient);
+        userProcessor.getOrganizationId = vi.fn().mockResolvedValue([{ orgId: 'org123', id: 'jira_org_org123' }]);
+        userProcessor.getParentId = vi.fn().mockResolvedValue('f2fd8d13-8bde-4ec0-bf87-4376fc2c8672');
+
+    });
+    afterEach(() => {
+        vi.resetAllMocks();
     });
 
-    const mockGetParentId = vi.fn().mockResolvedValue('cec133a0-5fe7-42cd-ad7b-4a794dcb38a7');
-    const mockNoParentId = vi.fn().mockResolvedValue('');
-
-    it('should process user data', async () => {
-
-        vi.setSystemTime(new Date('2023-08-29T00:00:00Z').toISOString());
-        // Create a new instance of the Users class with the mock data
-        const org = new UsersProcessor(mockData);
-        org.getParentId = mockGetParentId;
-
-        const mockValidate = mockUserValidate.mockReturnValue(false);
-        const mockProcessor = mockUserProcessor.mockResolvedValue(mockUser);
-        mockUserValidate = mockValidate;
-        mockUserProcessor = mockProcessor;
-
-        await handler(event);
-
-        expect(mockLoggerInfo).toHaveBeenCalledWith('Records Length: 2');
-        expect(mockUserValidate).toHaveBeenCalledTimes(2);
-        expect(mockUserProcessor).toHaveBeenCalledTimes(2);
-        expect(mockSendDataToQueue).toHaveBeenCalledTimes(2);
-        expect(mockLogProcessToRetry).not.toHaveBeenCalled();
-        expect(mockLoggerError).not.toHaveBeenCalled();
-        // Log the input data passed to the mockUserProcessor function
-        logger.info(mockUserProcessor.mock.calls);
+    describe('constructor', () => {
+        it('should create a new instance of User with the given properties', () => {
+            const constructUserProcessor = new UserProcessor(apiData);
+            expect(constructUserProcessor).toEqual({ apiData });
+        });
     });
 
 
+    // eslint-disable-next-line max-lines-per-function
+    describe('processor', async () => {
+
+        it('should throw an error if organization is not found', async () => {
+            const mockLoggerError = vi.spyOn(logger, 'error');
+            userProcessor.getOrganizationId = vi.fn().mockReturnValue([]);
+            await expect(userProcessor.processor()).rejects.toThrow('Organization not found');
+            expect(mockLoggerError).toHaveBeenCalledWith('Organization not found');
+        });
+        // eslint-disable-next-line max-lines-per-function
+        it('should return a user object with correct properties', async () => {
+            jiraClient.getUser = vi.fn().mockResolvedValue({
+                ...user,
+                groups: null,
+                applicationRoles: null,
+            });
+            const result = await userProcessor.processor();
+            expect(result.body.id).toBeDefined();
+            expect(result.body.userId).toBeDefined();
+            expect(result.body.userType).toBeDefined();
+            expect(result.body.emailAddress).toBeDefined();
+            expect(result.body.displayName).toBeDefined();
+            expect(result.body.avatarUrls).toBeDefined();
+            expect(result.body.isActive).toBeDefined();
+            expect(result.body.groups).toBeDefined();
+            expect(result.body.applicationRoles).toBeDefined();
+            expect(result.body.isDeleted).toBeDefined();
+            expect(result.body.deletedAt).toBeDefined();
+            expect(result.body.createdAt).toBeDefined();
+            expect(result.body.organizationId).toBeDefined();
+            expect(result).toEqual({
+                id: 'f2fd8d13-8bde-4ec0-bf87-4376fc2c8672',
+                body: {
+                    id: 'jira_user_user123',
+                    userId: 'user123',
+                    userType: Jira.Enums.UserType.Atlassian,
+                    emailAddress: 'user@example.com',
+                    displayName: 'User Example',
+                    avatarUrls: {
+                        avatarUrl48x48: 'https://example.com/avatar48.png',
+                        avatarUrl32x32: 'https://example.com/avatar32.png',
+                        avatarUrl24x24: 'https://example.com/avatar24.png',
+                        avatarUrl16x16: 'https://example.com/avatar16.png',
+                    },
+                    groups: null,
+                    applicationRoles: null,
+                    isActive: true,
+                    isDeleted: false,
+                    deletedAt: null,
+                    createdAt: '2022-01-01T00:00:00.000Z',
+                    organizationId: 'jira_org_org123',
+                },
+            });
+        });
+
+        it('should handle missing parent ID', async () => {
+            userProcessor.getParentId = vi.fn().mockResolvedValue('');
+            const result = await userProcessor.processor();
+            expect(result.id).not.toBe('f2fd8d13-8bde-4ec0-bf87-4376fc2c8672');
+            expect(result).toEqual({
+                id: expect.any(String),
+                body: {
+                    id: 'jira_user_user123',
+                    userId: 'user123',
+                    userType: 'atlassian',
+                    emailAddress: 'user@example.com',
+                    displayName: 'User Example',
+                    avatarUrls: {
+                        avatarUrl48x48: 'https://example.com/avatar48.png',
+                        avatarUrl32x32: 'https://example.com/avatar32.png',
+                        avatarUrl24x24: 'https://example.com/avatar24.png',
+                        avatarUrl16x16: 'https://example.com/avatar16.png',
+                    },
+                    isActive: true,
+                    groups: null,
+                    applicationRoles: null,
+                    isDeleted: false,
+                    deletedAt: null,
+                    createdAt: '2022-01-01T00:00:00.000Z',
+                    organizationId: 'jira_org_org123',
+                },
+            });
+        });
+    });
 });
