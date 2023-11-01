fb997ed41dba47c274e68f297c39405b48d77713
manasaggrawal <manas@studiographene.com>
2023-11-01 18:19:39 +0530

    feat(PX-932): marking sprints and issues and board delete along with project soft delete

--- packages/jira/src/repository/project/save-project.ts
+++ packages/jira/src/repository/project/save-project.ts
@@ -4,7 +4,7 @@ import { ElasticSearchClient } from '@pulse/elasticsearch';
 import { Jira } from 'abstraction';
 import { logger } from 'core';
 import { Config } from 'sst/node/config';
-import { searchedDataFormator } from '../../util/response-formatter';
+import { searchedDataFormator, searchedDataFormatorWithDeleted } from '../../util/response-formatter';
 import { ParamsMapping } from '../../model/params-mapping';
 import { mappingPrefixes } from '../../constant/config';
 
@@ -40,11 +40,41 @@ export async function saveProjectDetails(data: Jira.Type.Project): Promise<void>
       updatedData.id = formattedData._id;
     }
     await esClientObj.putDocument(Jira.Enums.IndexName.Project, updatedData);
+
+    if (data.body.isDeleted) {
+      await Promise.all([updateData(esClientObj, Jira.Enums.IndexName.Sprint, 'body.projectId', data.body.id, true),
+      updateData(esClientObj, Jira.Enums.IndexName.Issue, 'body.projectId', data.body.id, true),
+      updateData(esClientObj, Jira.Enums.IndexName.Board, 'body.projectId', data.body.id, true)])
+    }
     logger.info('saveProjectDetails.successful');
   } catch (error: unknown) {
     logger.error('saveProjectDetails.error', {
       error,
     });
     throw error;
+  }
+}
+
+
+/**
+ * Updates data in ElasticSearch index based on the provided matchField and matchValue.
+ * @param esClientObj - The ElasticSearch client object.
+ * @param indexName - The name of the ElasticSearch index.
+ * @param matchField - The field to match against in the ElasticSearch index.
+ * @param matchValue - The value to match against in the ElasticSearch index.
+ * @param isDeleted - Optional flag to mark the data as deleted.
+ * @returns Promise<void>
+ */
+async function updateData(esClientObj: ElasticSearchClient, indexName: string, matchField: string, matchValue: string, isDeleted = false) {
+  const matchQry = esb.matchQuery(matchField, matchValue).toJSON();
+  const data = await esClientObj.searchWithEsb(indexName, matchQry);
+  const [formattedData] = await searchedDataFormatorWithDeleted(data);
+  if (formattedData) {
+    if (isDeleted) {
+      formattedData.isDeleted = true;
+    }
+    const { _id: id, ...body } = formattedData;
+    await esClientObj.putDocument(indexName, { id, body });
+    logger.info(`save${indexName}Details.successful`);
   }
 }
