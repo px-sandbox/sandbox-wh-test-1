74b532234e1accd6df750b06899d271a3a53d624
charchit khandelwal <charchit.khandelwal@studiographene.com>
2023-11-16 18:13:25 +0530

    feat(PX-977): added sorting and check for dist key
    Signed-off-by: charchit khandelwal <charchit.khandelwal@studiographene.com>

--- packages/github/src/util/node-library-info.ts
+++ packages/github/src/util/node-library-info.ts
@@ -1,14 +1,21 @@
-import { logger } from "core";
-import axios from "axios";
-import { LibInfo } from "abstraction/github/type";
+import { logger } from 'core';
+import axios from 'axios';
+import { LibInfo } from 'abstraction/github/type';
+import _ from 'lodash';
 
 export async function getNodeLibInfo(libName: string, currentVersion?: string): Promise<LibInfo> {
     const npmPackageUrl = `https://registry.npmjs.org/${libName}`;
+    let latestVersion: string;
     try {
         logger.info('get-library-info.invoked', { libName });
         const { data } = await axios.get(npmPackageUrl);
-        const versionArray = Object.keys(data.time);
-        const latestVersion = versionArray[versionArray.length - 1];
+
+        if (data['dist-tags'] && data['dist-tags'].latest) {
+            latestVersion = data['dist-tags'].latest;
+        } else {
+            const filteredDataTime = _.omit(data.time, ['modified', 'created']);
+            [latestVersion] = _.sortBy(Object.keys(filteredDataTime)).reverse();
+        }
 
         return {
             name: data.name,
@@ -19,8 +26,8 @@ export async function getNodeLibInfo(libName: string, currentVersion?: string): 
             current: {
                 version: currentVersion ?? latestVersion,
                 releaseDate: data.time[currentVersion ?? latestVersion],
-            }
-        }
+            },
+        };
     } catch (error) {
         logger.error('get-library-info.error', { errorInfo: JSON.stringify(error) });
         throw error;
