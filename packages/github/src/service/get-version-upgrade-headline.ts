e64097794cb01945dc1afae222e35701b07551c6
charchit khandelwal <charchit.khandelwal@studiographene.com>
2023-11-24 15:50:43 +0530

    fix(PX-981): compare based on range in verion headline
    Signed-off-by: charchit khandelwal <charchit.khandelwal@studiographene.com>

--- packages/github/src/service/get-version-upgrade-headline.ts
+++ packages/github/src/service/get-version-upgrade-headline.ts
@@ -5,28 +5,40 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { HttpStatusCode, logger, responseParser } from 'core';
 import esb from 'elastic-builder';
 import { Config } from 'sst/node/config';
+import moment from 'moment';
 import { LibParamsMapping } from '../model/lib-master-mapping';
 import { searchedDataFormator } from '../util/response-formatter';
 
+
+function compare(operator: string, value: number, latestReleaseDate: string, currReleaseDate: string): boolean {
+    const diffInDays = moment(latestReleaseDate).diff(moment(currReleaseDate), 'months');
+    switch (operator) {
+        case '<': return diffInDays < value;
+        case '<=': return diffInDays <= value;
+        default: return false;
+    }
+}
 /**
  *  Considering that single repo will have less than 1000 libraries
  */
 const getLibFromDB = async (
-    libNameAndVersion: { libName: string; version: string }[]
+    libNameAndVersion: { libName: string; version: string, releaseDate: string }[],
+    range: string
 ): Promise<{ countOutOfDateLib: number; countUpToDateLib: number }> => {
     let countOutOfDateLib = 0;
     let countUpToDateLib = 0;
-
+    const [operator, value] = range.split(' ');
     const promises = libNameAndVersion.map(async (lib) => {
         const records = await new DynamoDbDocClient().find(
             new LibParamsMapping().prepareGetParams(lib.libName)
         );
         if (records && records.version) {
-            const latestVer = records.version as string;
-            if (latestVer !== lib.version) {
-                countOutOfDateLib += 1;
-            } else {
+            // const latestVer = records.version as string;
+            if (records.releaseDate && compare(operator, parseInt(value, 10),
+                String(records.releaseDate), lib.releaseDate)) {
                 countUpToDateLib += 1;
+            } else {
+                countOutOfDateLib += 1;
             }
         }
     });
@@ -35,14 +47,15 @@ const getLibFromDB = async (
 };
 
 const getLibFromES = async (
-    repoIds: string[]
+    repoIds: string[],
+    range: string
 ): Promise<{
     countOutOfDateLib: number;
     countUpToDateLib: number;
 }> => {
     try {
-        let libData = [];
-        let size = 100;
+        const libData = [];
+        const size = 100;
         let from = 0;
         const esClientObj = new ElasticSearchClient({
             host: Config.OPENSEARCH_NODE,
@@ -62,22 +75,22 @@ const getLibFromES = async (
                 .toJSON() as { query: object };
 
 
-            const esLibData = await esClientObj.searchWithEsb(
+            const esLibData = esClientObj.searchWithEsb(
                 Github.Enums.IndexName.GitRepoLibrary,
                 query.query
             );
-            libData.push(esLibData)
+            const libFormatData = searchedDataFormator(esLibData);
+            libData.push(...libFormatData)
             from += size;
         }
         while (libData.length > 100)
-
-        const data = await searchedDataFormator(libData);
-        const libNameAndVersion = data.map((lib: { libName: string; version: string }) => ({
+        const libNameAndVersion = libData.map((lib: { libName: string; version: string, releaseDate: string }) => ({
             libName: lib.libName,
             version: lib.version,
+            releaseDate: lib.releaseDate
         }));
         logger.info('LIB_NAME_AND_VERSION', libNameAndVersion);
-        return getLibFromDB(libNameAndVersion);
+        return getLibFromDB(libNameAndVersion, range);
     } catch (error) {
         logger.error('getLibFromES.error', error);
         throw error;
@@ -86,7 +99,8 @@ const getLibFromES = async (
 
 export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
     const repoIds: string[] = event.queryStringParameters?.repoIds?.split(',') || [''];
-    const lib = await getLibFromES(repoIds);
+    const range = event.queryStringParameters?.range || '<= 1';
+    const lib = await getLibFromES(repoIds, range);
     return responseParser
         .setBody({ headline: lib })
         .setMessage('Headline for version upgrade')
