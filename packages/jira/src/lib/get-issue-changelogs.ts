a3ae57dd61b2e77f97e4f19af9ef1f33c4e1e6c6
charchit khandelwal <charchit.khandelwal@studiographene.com>
2024-01-08 18:38:01 +0530

    fix(PX-1097): changelog issue
    Signed-off-by: charchit khandelwal <charchit.khandelwal@studiographene.com>

--- packages/jira/src/lib/get-issue-changelogs.ts
+++ packages/jira/src/lib/get-issue-changelogs.ts
@@ -1,21 +1,21 @@
 import { Jira } from 'abstraction';
+import { ChangelogField } from 'abstraction/jira/enums';
 import { JiraClient } from './jira-client';
-import { ChangelogField, ChangelogStatus } from 'abstraction/jira/enums';
 
 export async function getIssueChangelogs(
     organization: string,
     issueId: string,
     jira: JiraClient
-): Promise<Array<Jira.ExternalType.Api.Changelogs>> {
+): Promise<Array<Jira.ExternalType.Webhook.ChangelogItem>> {
     const changelogs = await jira.getIssueChangelogs(issueId);
 
-    const changelogValues: Jira.ExternalType.Api.Changelogs[] = changelogs.flatMap((changelog) => {
+    const changelogValues: Jira.ExternalType.Webhook.ChangelogItem[] = changelogs.flatMap((changelog) => {
         const items = changelog.items.filter(
             (item) =>
                 (item.fieldtype === ChangelogField.JIRA && item.field === ChangelogField.STATUS) ||
                 (item.fieldtype === ChangelogField.CUSTOM && item.field === ChangelogField.SPRINT
                 ));
-        return { items };
+        return items;
     });
     return changelogValues;
 }
