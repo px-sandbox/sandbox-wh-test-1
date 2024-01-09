dbce06d02a9c232a6bd4271f5302b3dbee4b3662
Jatin Raghav <jatin.raghav@studiographene.com>
2024-01-09 14:48:43 +0530

    feat(PX-1101): update px status

--- packages/abstraction/jira/enums/issues.ts
+++ packages/abstraction/jira/enums/issues.ts
@@ -7,11 +7,11 @@ export enum IssuesTypes {
 export enum ChangelogStatus {
   QA_FAILED = 'QA_Failed',
   READY_FOR_QA = 'Ready_For_QA',
-  Ready_For_UAT = 'Ready_For_UAT',
+  READY_FOR_UAT = 'Ready_For_UAT',
   QA_PASSED = 'QA_Passed',
-  Ready_For_Prod = 'Ready_For_Prod',
-  QA_Pass_Deploy = 'QA_Pass_Deploy',
-  Done = 'Done',
+  READY_FOR_PROD = 'Ready_For_Prod',
+  QA_PASS_DEPLOY = 'QA_Pass_Deploy',
+  DONE = 'Done',
 }
 export enum ChangelogField {
   SPRINT = 'Sprint',
