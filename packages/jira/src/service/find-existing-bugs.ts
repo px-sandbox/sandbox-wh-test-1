e1601404b287729955351c6898356d45c21d89c3
Jatin Raghav <jatin.raghav@studiographene.com>
2024-01-09 16:12:30 +0530

    feat(PX-1101): changes for debugging on sandbox

--- packages/jira/src/service/find-existing-bugs.ts
+++ packages/jira/src/service/find-existing-bugs.ts
@@ -1,13 +1,14 @@
+/* eslint-disable max-lines-per-function */
 /* eslint-disable no-await-in-loop */
 import { ElasticSearchClient } from "@pulse/elasticsearch";
 import { Jira, Other } from "abstraction";
-import { logger } from "core";
 import { Config } from "sst/node/config";
 import esb from "elastic-builder";
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { SQSClient } from "@pulse/event-handler";
 import { Queue } from "sst/node/queue";
 import { mappingPrefixes } from "src/constant/config";
+import { HttpStatusCode, logger, responseParser } from 'core';
 import { searchedDataFormator } from "../util/response-formatter";
 import { getOrganizationById } from "../repository/organization/get-organization";
 
@@ -82,6 +83,14 @@ export const handler = async function getIssuesList(event: APIGatewayProxyEvent)
             }
             return new SQSClient().sendMessage(formattedBug, Queue.qReOpenRateMigrator.queueUrl);
         }))
+
+        return responseParser
+            .setBody({ message: 'Existing bugs fetched successfully for reopen' })
+            .setMessage('Existing bugs fetched successfully for reopen')
+            .setStatusCode(HttpStatusCode[200])
+            .setResponseBodyCode('SUCCESS')
+            .send();
+
 
     } catch (error) {
         logger.error('get existing bug for reopen error', { error });
