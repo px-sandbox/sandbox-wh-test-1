72a2b45c23e39508a10ade0af45050c5271612fd
charchit khandelwal <charchit.khandelwal@studiographene.com>
2024-01-12 15:56:17 +0530

    feat(PX-1000): added logger inside changelogs
    Signed-off-by: charchit khandelwal <charchit.khandelwal@studiographene.com>

--- packages/jira/src/lib/get-issue-changelogs.ts
+++ packages/jira/src/lib/get-issue-changelogs.ts
@@ -1,12 +1,14 @@
 import { Jira } from 'abstraction';
 import { ChangelogField } from 'abstraction/jira/enums';
 import { JiraClient } from './jira-client';
+import { logger } from 'core';
 
 export async function getIssueChangelogs(
     organization: string,
     issueId: string,
     jira: JiraClient
 ): Promise<Array<Jira.ExternalType.Webhook.ChangelogItem>> {
+    logger.info('getIssueChangelogs.file', { issueId, jira });
     const changelogs = await jira.getIssueChangelogs(issueId);
 
     const changelogValues: Jira.ExternalType.Webhook.ChangelogItem[] = changelogs.flatMap((changelog) => {
