6e5de3b078708d647c4fb152b97eccd331b893a2
manasaggrawal <manas@studiographene.com>
2024-02-13 14:50:19 +0530

    feat(PX-1178): added estimates vs actuals route in pm stack

--- stacks/pm/route.ts
+++ stacks/pm/route.ts
@@ -1,13 +1,19 @@
 import { ApiRouteProps } from 'sst/constructs';
 
-export function initializeRoutes(
-): Record<string, ApiRouteProps<'universal'>> {
+export function initializeRoutes(): Record<string, ApiRouteProps<'universal'>> {
   return {
-
-    // GET PR comments graph data
+    // GET sprint variance data
     'GET /jira/sprint/variance': {
       function: 'packages/jira/src/service/sprint-variance.handler',
       authorizer: 'universal',
-    }
-  }
+    },
+
+    // GET estimates vs actuals breakdown
+    'GET /jira/{projectId}/{sprintId}/metrics/pm/estimates-vs-actuals': {
+      function: {
+        handler: 'packages/jira/src/service/issue/estimates-vs-actuals-breakdown.handler',
+      },
+      authorizer: 'universal',
+    },
+  };
 }
