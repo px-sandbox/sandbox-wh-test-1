070bd26e0eab7db8aa1b952f599f7aedd361817e
manasaggrawal <manas@studiographene.com>
2024-02-29 16:11:16 +0530

    feat(PX-1263): reopen rate delete migration

---
+++ packages/jira/src/migrations/issue-delete-reopen-rate.ts
@@ -0,0 +1,127 @@
+/* eslint-disable no-await-in-loop */
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { logger } from 'core';
+import { ElasticSearchClient } from '@pulse/elasticsearch';
+import { Config } from 'sst/node/config';
+import esb from 'elastic-builder';
+import { Jira, Other } from 'abstraction';
+import moment from 'moment';
+import { searchedDataFormator } from '../util/response-formatter';
+import { getOrganization } from '../repository/organization/get-organization';
+
+const esClientObj = new ElasticSearchClient({
+  host: Config.OPENSEARCH_NODE,
+  username: Config.OPENSEARCH_USERNAME ?? '',
+  password: Config.OPENSEARCH_PASSWORD ?? '',
+});
+
+/**
+ * Fetches reopen rate data for the specified project, issue keys, and organization.
+ * Updates the isDeleted and deletedAt properties of the fetched data.
+ * @param projectId The ID of the project.
+ * @param issueKeys An array of issue keys.
+ * @param orgId The ID of the organization.
+ * @returns A Promise that resolves when the update is complete.
+ */
+async function fetchReopenRateData(
+  projectId: string,
+  issueKeys: string[],
+  orgId: string
+): Promise<void> {
+  try {
+    const updateReopenRateDataQuery = esb
+      .boolQuery()
+      .must([
+        esb.termsQuery('body.issueKey.keyword', issueKeys),
+        esb.termQuery('body.projectId', projectId),
+        esb.termQuery('body.organizationId', orgId),
+        esb.termQuery('body.isDeleted', false),
+      ])
+      .toJSON();
+
+    const script = esb
+      .script()
+      .source(
+        'ctx._source.body.isDeleted=params.isDeleted;ctx._source.body.deletedAt=params.deletedAt'
+      )
+      .params({
+        deletedAt: moment().toISOString(),
+        isDeleted: true,
+      })
+      .toJSON();
+
+    await esClientObj.updateByQuery(
+      Jira.Enums.IndexName.ReopenRate,
+      updateReopenRateDataQuery,
+      script
+    );
+  } catch (error) {
+    logger.error(`issue-delete-reopen-rate.error: ${error}`);
+    throw error;
+  }
+  logger.info('issue-delete-reopen-rate-migration:success');
+}
+/**
+ * Handles the issue delete reopen rate migration.
+ *
+ * @param event - The API Gateway proxy event.
+ * @returns A Promise that resolves to void.
+ * @throws Error if projectId is not provided or if the organization is not found.
+ */
+export const handler = async function issueDeleteReopenRateMigration(
+  event: APIGatewayProxyEvent
+): Promise<void> {
+  const projectId = event?.queryStringParameters?.projectId;
+  if (!projectId) {
+    throw new Error('projectId is required');
+  }
+  const organization = event?.queryStringParameters?.organization ?? '';
+  const orgData = await getOrganization(organization);
+
+  if (!orgData) {
+    logger.error(`Organization ${organization} not found`);
+    throw new Error(`Organization ${organization} not found`);
+  }
+
+  try {
+    const issueQuery = esb
+      .requestBodySearch()
+      .query(
+        esb
+          .boolQuery()
+          .must([
+            esb.termQuery('body.organizationId', `${orgData.id}`),
+            esb.termQuery('body.projectId', `${projectId}`),
+            esb.termQuery('body.isDeleted', true),
+          ])
+      )
+      .source(['body.issueKey'])
+      .size(1000)
+      .sort(esb.sort('_id'));
+
+    let unformattedIssueData: Other.Type.HitBody = await esClientObj.esbRequestBodySearch(
+      Jira.Enums.IndexName.Issue,
+      issueQuery.toJSON()
+    );
+    const issueData = [];
+    let formattedIssueData = await searchedDataFormator(unformattedIssueData);
+    issueData.push(...formattedIssueData);
+
+    while (formattedIssueData?.length > 0) {
+      const lastHit = unformattedIssueData.hits.hits[unformattedIssueData.hits.hits.length - 1];
+      const requestBodyQuery = issueQuery.searchAfter([lastHit.sort[0]]).toJSON();
+      unformattedIssueData = await searchedDataFormator(
+        await esClientObj.esbRequestBodySearch(Jira.Enums.IndexName.Issue, requestBodyQuery)
+      );
+      formattedIssueData = await searchedDataFormator(unformattedIssueData);
+      issueData.push(...formattedIssueData);
+    }
+
+    const issueKeys = [...new Set(issueData.map((issue) => issue.issueKey))];
+    logger.info('issue-delete-reopen-rate: issueKeys: ', issueKeys);
+    await fetchReopenRateData(projectId, issueKeys, `${orgData.id}`);
+  } catch (error) {
+    logger.error(`issue-delete-reopen-rate.error:  ${error}`);
+    throw error;
+  }
+};
--- packages/jira/src/repository/organization/get-organization.ts
+++ packages/jira/src/repository/organization/get-organization.ts
@@ -9,24 +9,20 @@ import { searchedDataFormator } from '../../util/response-formatter';
  * @returns A Promise that resolves to an array of objects containing the organization ID and body,
  *  or an empty array if no organization was found.
  */
-export async function getOrganization(orgName: string):
-    Promise<(Pick<Other.Type.Hit, "_id"> & Other.Type.HitBody) | undefined> {
-    const _esClient = new ElasticSearchClient({
-        host: Config.OPENSEARCH_NODE,
-        username: Config.OPENSEARCH_USERNAME ?? '',
-        password: Config.OPENSEARCH_PASSWORD ?? '',
-    });
-    const organization = await _esClient.search(
-        Jira.Enums.IndexName.Organization,
-        'name',
-        orgName
-    );
+export async function getOrganization(
+  orgName: string
+): Promise<(Pick<Other.Type.Hit, '_id'> & Other.Type.HitBody) | undefined> {
+  const _esClient = new ElasticSearchClient({
+    host: Config.OPENSEARCH_NODE,
+    username: Config.OPENSEARCH_USERNAME ?? '',
+    password: Config.OPENSEARCH_PASSWORD ?? '',
+  });
+  const organization = await _esClient.search(Jira.Enums.IndexName.Organization, 'name', orgName);
 
-    const [orgData] = await searchedDataFormator(organization);
+  const [orgData] = await searchedDataFormator(organization);
 
-    return orgData;
+  return orgData;
 }
-
 
 /**
  * Retrieves the organization ID for a given organization name.
@@ -36,17 +32,13 @@ export async function getOrganization(orgName: string):
  */
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export async function getOrganizationById(orgId: string) {
-    const _esClient = new ElasticSearchClient({
-        host: Config.OPENSEARCH_NODE,
-        username: Config.OPENSEARCH_USERNAME ?? '',
-        password: Config.OPENSEARCH_PASSWORD ?? '',
-    });
-    const organization = await _esClient.search(
-        Jira.Enums.IndexName.Organization,
-        'id',
-        orgId
-    );
+  const _esClient = new ElasticSearchClient({
+    host: Config.OPENSEARCH_NODE,
+    username: Config.OPENSEARCH_USERNAME ?? '',
+    password: Config.OPENSEARCH_PASSWORD ?? '',
+  });
+  const organization = await _esClient.search(Jira.Enums.IndexName.Organization, 'id', orgId);
 
-    const orgData = await searchedDataFormator(organization);
-    return orgData;
+  const orgData = await searchedDataFormator(organization);
+  return orgData;
 }
