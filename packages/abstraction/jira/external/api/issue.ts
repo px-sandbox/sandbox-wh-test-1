b2fd6d76e5dbfc2a9420e0ff959f90f3959a0254
manasaggrawal <manas@studiographene.com>
2024-03-01 13:17:25 +0530

    feat(PX-1263): parent and subtask linking in issue formatter

--- packages/abstraction/jira/external/api/issue.ts
+++ packages/abstraction/jira/external/api/issue.ts
@@ -44,5 +44,43 @@ export type Issue = {
       }
     ];
     summary: string;
+    parent: {
+      id: string;
+      key: string;
+      self: string;
+      fields: {
+        summary: string;
+        status: {
+          self: string;
+          description: string;
+          iconUrl: string;
+          name: string;
+          id: string;
+          statusCategory: {
+            self: string;
+            id: number;
+            key: string;
+            colorName: string;
+            name: string;
+          };
+        };
+        priority: {
+          self: string;
+          iconUrl: string;
+          name: string;
+          id: string;
+        };
+        issuetype: {
+          self: string;
+          id: string;
+          description: string;
+          iconUrl: string;
+          name: string;
+          subtask: boolean;
+          avatarId: number;
+          hierarchyLevel: number;
+        };
+      };
+    };
   };
 };
