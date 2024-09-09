6da064fa10857292c7539eb22ed30eea85369033
charchit khandelwal <charchit.khandelwal@studiographene.com>
2024-09-09 15:34:01 +0530

    feat(PX-1786): branch test cases
    Signed-off-by: charchit khandelwal <charchit.khandelwal@studiographene.com>

--- packages/github/src/processors/test/branch.test.ts
+++ packages/github/src/processors/test/branch.test.ts
@@ -1,125 +1,185 @@
-import { Github } from 'abstraction';
-import { Config } from 'sst/node/config';
-import moment from 'moment';
-import { expect, it, vi, describe } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { BranchProcessor } from '../branch';
 
-const mockData = {
-  id: '123',
-  name: 'my-branch',
-  ref: 'refs/heads/my-branch',
-  repo_id: '456',
-  created_at: '2022-01-01T00:00:00Z',
-  pushed_at: '2022-01-01T00:00:00Z',
-  updated_at: '2022-01-01T00:00:00Z',
-  action: 'initialized',
-  organization_id: '789',
-  protected: false,
-} as Github.ExternalType.Api.Branch;
+export function generateuniqIds() {
+  return '94cc22e3-824b-48d5-8df7-12a9c613596b';
+}
 
-// No branch name
-const mockData1 = {
-  id: '123',
-  ref: 'refs/heads/my-branch',
-  repo_id: '456',
-  created_at: '2022-01-01T00:00:00Z',
-  pushed_at: '2022-01-01T00:00:00Z',
-  updated_at: '2022-01-01T00:00:00Z',
-  action: 'initialized',
-  organization_id: '789',
-  protected: false,
-} as Github.ExternalType.Api.Branch;
-
-vi.mock('src/constant/config', (mockMappingPrefixes) => ({
-  mappingPrefixes: mockMappingPrefixes,
-}));
-vi.mock('src/model/params-mapping', (mockParamsMapping) => ({
-  ParamsMapping: mockParamsMapping,
-}));
+const mockData: any = {
+  data: {
+    commit: {
+      sha: '935d9b2759d73b8622c9527dd1bc40d433b25c84',
+      url: 'https://api.github.com/repos/unofficial-samsung/s23/commits/935d9b2759d73b8622c9527dd1bc40d433b25c84',
+    },
+    id: 'ODQyMzMyNjE5X2NoYXJjaGl0RGV2LXBhdGNoLTI=',
+    name: 'charchitDev-patch-2',
+    orgId: '178363207',
+    protected: false,
+    repo_id: 842332619,
+  },
+  level: 'info',
+  message: 'processAndStoreSQSRecord.info: BRANCH_SQS_RECEIVER_HANDLER',
+  requestId: 'dRCdqi4fjoEEPWA=',
+  timestamp: '2024-08-29T10:17:38.638Z',
+};
 const mockGetParentId = vi.fn().mockResolvedValue('94cc22e3-824b-48d5-8df7-12a9c613596b');
-vi.mock('src/constant/config', () => ({
-  mappingPrefixes: 'gh_branch',
-}));
-// eslint-disable-next-line max-lines-per-function
+vi.setSystemTime(new Date('2024-09-04T00:00:00Z').toISOString());
+
 describe('Branch', async () => {
-  // Create a new instance of the BranchProcessor class with the mock data
   it('should process the branch data correctly', async () => {
-    vi.setSystemTime(new Date('2023-08-29T00:00:00Z').toISOString());
-    const processor = new BranchProcessor(mockData);
-
-    // Mock the getParentId method
+    const processor = new BranchProcessor(
+      'create',
+      mockData.data,
+      '',
+      mockData.data.id,
+      mockData.data.name
+    );
     processor.getParentId = mockGetParentId;
-
-    // Mock the Config module
-    Config.GIT_ORGANIZATION_ID = 'my-organization-id';
-
-    // Call the processor method and get the output object
-    const output = await processor.processor();
-    const data = output.body;
-
-    // Check that the data object has the correct properties and values
-    expect(data).toEqual({
-      id: 'undefined_123',
-      githubBranchId: '123',
-      name: 'my-branch',
-      organizationId: 'undefined_my-organization-id',
-      repoId: 'undefined_456',
-      createdAt: '2022-01-01T00:00:00Z',
-      pushedAt: '2022-01-01T00:00:00Z',
-      updatedAt: '2022-01-01T00:00:00Z',
-      deletedAt: undefined,
-      isDeleted: false,
-      action: [
-        {
-          action: 'initialized',
-          actionTime: new Date().toISOString(),
-          actionDay: moment().format('dddd'),
-        },
-      ],
-      createdAtDay: moment('2022-01-01T00:00:00Z').format('dddd'),
-      computationalDate: moment('2022-01-03T00:00:00Z').format('YYYY-MM-DD'),
-      githubDate: moment('2022-01-01T00:00:00Z').format('YYYY-MM-DD'),
-      protected: false,
+    await processor.process();
+    const output = processor.formattedData;
+    expect(output).toEqual({
+      id: '94cc22e3-824b-48d5-8df7-12a9c613596b',
+      body: {
+        id: 'gh_branch_ODQyMzMyNjE5X2NoYXJjaGl0RGV2LXBhdGNoLTI=',
+        githubBranchId: 'ODQyMzMyNjE5X2NoYXJjaGl0RGV2LXBhdGNoLTI=',
+        name: 'charchitDev-patch-2',
+        organizationId: 'gh_org_178363207',
+        repoId: 'gh_repo_842332619',
+        createdAt: new Date('2024-09-04T00:00:00.000Z'),
+        pushedAt: undefined,
+        updatedAt: undefined,
+        deletedAt: null,
+        isDeleted: false,
+        action: [
+          {
+            action: 'create',
+            actionTime: '2024-09-04T00:00:00.000Z',
+            actionDay: 'Wednesday',
+          },
+        ],
+        createdAtDay: 'Wednesday',
+        computationalDate: '2024-09-04',
+        githubDate: '2024-09-04',
+        protected: false,
+      },
+    });
+  });
+  //Test when processor method is called and no parent ID is found
+  it('should process the branch data correctly when no parent ID is found', async () => {
+    const processor = new BranchProcessor('create', mockData.data, '', mockData.id, mockData.name);
+    processor.getParentId = vi.fn().mockResolvedValue(null);
+    vi.mock('uuid', () => ({
+      v4: vi.fn(() => generateuniqIds()),
+    }));
+    processor.putDataToDynamoDB = vi.fn().mockResolvedValue(generateuniqIds());
+    await processor.process();
+    const output = processor.formattedData;
+    expect(output).toEqual({
+      id: generateuniqIds(),
+      body: {
+        id: 'gh_branch_ODQyMzMyNjE5X2NoYXJjaGl0RGV2LXBhdGNoLTI=',
+        githubBranchId: 'ODQyMzMyNjE5X2NoYXJjaGl0RGV2LXBhdGNoLTI=',
+        name: 'charchitDev-patch-2',
+        organizationId: 'gh_org_178363207',
+        repoId: 'gh_repo_842332619',
+        createdAt: new Date('2024-09-04T00:00:00.000Z'),
+        pushedAt: undefined,
+        updatedAt: undefined,
+        deletedAt: null,
+        isDeleted: false,
+        action: [
+          {
+            action: 'create',
+            actionTime: '2024-09-04T00:00:00.000Z',
+            actionDay: 'Wednesday',
+          },
+        ],
+        createdAtDay: 'Wednesday',
+        computationalDate: '2024-09-04',
+        githubDate: '2024-09-04',
+        protected: false,
+      },
     });
   });
 
-  it('should not process the missing branch name', async () => {
-    vi.setSystemTime(new Date('2023-08-29T00:00:00Z').toISOString());
-    const processor = new BranchProcessor(mockData1);
-
-    // Mock the getParentId method
+  //Test when branch is deleted
+  it('should process the branch data correctly when branch is deleted', async () => {
+    const processor = new BranchProcessor(
+      'delete',
+      mockData.data,
+      '',
+      mockData.data.id,
+      mockData.data.name
+    );
     processor.getParentId = mockGetParentId;
-
-    // Mock the Config module
-    Config.GIT_ORGANIZATION_ID = 'my-organization-id';
-
-    // Call the processor method and get the output object
-    const output = await processor.processor();
-    const data = output.body;
-
-    // Check that the data object has the correct properties and values
-    expect(data).toEqual({
-      id: 'undefined_123',
-      githubBranchId: '123',
-      organizationId: 'undefined_my-organization-id',
-      repoId: 'undefined_456',
-      createdAt: '2022-01-01T00:00:00Z',
-      pushedAt: '2022-01-01T00:00:00Z',
-      updatedAt: '2022-01-01T00:00:00Z',
-      deletedAt: undefined,
-      isDeleted: false,
-      action: [
-        {
-          action: 'initialized',
-          actionTime: new Date().toISOString(),
-          actionDay: moment().format('dddd'),
-        },
-      ],
-      createdAtDay: moment('2022-01-01T00:00:00Z').format('dddd'),
-      computationalDate: moment('2022-01-03T00:00:00Z').format('YYYY-MM-DD'),
-      githubDate: moment('2022-01-01T00:00:00Z').format('YYYY-MM-DD'),
-      protected: false,
+    await processor.process();
+    const output = processor.formattedData;
+    expect(output).toEqual({
+      id: '94cc22e3-824b-48d5-8df7-12a9c613596b',
+      body: {
+        id: 'gh_branch_ODQyMzMyNjE5X2NoYXJjaGl0RGV2LXBhdGNoLTI=',
+        githubBranchId: 'ODQyMzMyNjE5X2NoYXJjaGl0RGV2LXBhdGNoLTI=',
+        name: 'charchitDev-patch-2',
+        organizationId: 'gh_org_178363207',
+        repoId: 'gh_repo_842332619',
+        createdAt: new Date('2024-09-04T00:00:00.000Z'),
+        pushedAt: undefined,
+        updatedAt: undefined,
+        deletedAt: '2024-09-04T00:00:00.000Z',
+        isDeleted: true,
+        action: [
+          {
+            action: 'delete',
+            actionTime: '2024-09-04T00:00:00.000Z',
+            actionDay: 'Wednesday',
+          },
+        ],
+        createdAtDay: 'Wednesday',
+        computationalDate: '2024-09-04',
+        githubDate: '2024-09-04',
+        protected: false,
+      },
     });
   });
-  // Check that the getParentId method was called with the correct arguments
+
+  //Test when branch is updated
+  it('should process the branch data correctly when branch is deleted', async () => {
+    mockData.data.name = 'charchitDev-patch-3';
+    const processor = new BranchProcessor(
+      'delete',
+      mockData.data,
+      '',
+      mockData.data.id,
+      mockData.data.name
+    );
+    processor.getParentId = mockGetParentId;
+    await processor.process();
+    const output = processor.formattedData;
+    expect(output).toEqual({
+      id: '94cc22e3-824b-48d5-8df7-12a9c613596b',
+      body: {
+        id: 'gh_branch_ODQyMzMyNjE5X2NoYXJjaGl0RGV2LXBhdGNoLTI=',
+        githubBranchId: 'ODQyMzMyNjE5X2NoYXJjaGl0RGV2LXBhdGNoLTI=',
+        name: 'charchitDev-patch-3',
+        organizationId: 'gh_org_178363207',
+        repoId: 'gh_repo_842332619',
+        createdAt: new Date('2024-09-04T00:00:00.000Z'),
+        pushedAt: undefined,
+        updatedAt: undefined,
+        deletedAt: '2024-09-04T00:00:00.000Z',
+        isDeleted: true,
+        action: [
+          {
+            action: 'delete',
+            actionTime: '2024-09-04T00:00:00.000Z',
+            actionDay: 'Wednesday',
+          },
+        ],
+        createdAtDay: 'Wednesday',
+        computationalDate: '2024-09-04',
+        githubDate: '2024-09-04',
+        protected: false,
+      },
+    });
+  });
 });
