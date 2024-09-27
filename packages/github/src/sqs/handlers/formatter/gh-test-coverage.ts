4c5ed477b136f650789fd599be8c9e40f8ec5371
JaskeeratSG <jaskeerat.chhabra@studiographene.com>
2024-09-27 18:12:57 +0530

    feat(PX-1877): extracting data from s3

--- packages/github/src/sqs/handlers/formatter/gh-test-coverage.ts
+++ packages/github/src/sqs/handlers/formatter/gh-test-coverage.ts
@@ -1,42 +1,36 @@
 import { SQSEvent, SQSRecord } from 'aws-lambda';
 import { logger } from 'core';
-import { logProcessToRetry } from 'rp';
-import { Queue } from 'sst/node/queue';
 import AWS from 'aws-sdk';
+import {fetchDataFromS3} from "../../../util/test-coverage"
 
-const s3 = new AWS.S3(); 
-
-const getS3File = async (key : string) => {
-    const params = {
-      Bucket: `${process.env.SST_STAGE}-test-coverage-report`, 
-      Key: key
-    };
-    console.log(key);
-    const s3Object = await s3.getObject(params).promise();
-    
-    if (s3Object.Body) {
-      return JSON.parse(s3Object.Body.toString('utf-8'));
-    } else {
-      throw new Error("S3 object body is undefined or empty");
-    }
-}
 
 
 export async function handler(event: SQSEvent): Promise<void> {
      await Promise.all(
      event.Records.map(async (record: SQSRecord) => {
       try{
-            const { organisationId, repoId, createdAt, key } = JSON.parse(record.body);
-      
-            console.log(`Received payload:`, { organisationId, repoId, createdAt, key });
-            const s3Data = await getS3File(key);
+            const { 
+              reqCtx: { requestId, resourceId },
+              organisationId, repoId, createdAt, s3ObjKey } = JSON.parse(record.body);
 
-            
-            console.log('S3 Data:', s3Data);
-            return {
-              statusCode: 200,
-              body: JSON.stringify({ message: "Processing successful" }),
-            };
+            const bucketName= `${process.env.SST_STAGE}-test-coverage-report`
+
+            const data = await fetchDataFromS3(s3ObjKey, bucketName, {
+              requestId,
+              resourceId,
+            });
+
+            if(!data){
+              logger.error({
+                message: 'ghTestCoverage.nodata',
+                error: 'No data received from s3 ',
+                requestId,
+                resourceId,
+              });
+            }
+            else{
+              console.log(data);
+            }
        }
        catch(error)
        {
