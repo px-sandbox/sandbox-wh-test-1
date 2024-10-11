a7fc18b2e64627be1b83ccb3664f9e55e59cc1c6
Kaushal965 <kaushal.mittal@studiographene.com>
2024-10-11 17:03:58 +0530

    feat(PX-1849): changes added

--- packages/github/src/cron/test-coverage.ts
+++ packages/github/src/cron/test-coverage.ts
@@ -1,58 +1,52 @@
 import { ElasticSearchClient } from '@pulse/elasticsearch';
-import { SQSClient } from '@pulse/event-handler';
 import { Github } from 'abstraction';
-import { APIGatewayProxyEvent } from 'aws-lambda';
 import { logger } from 'core';
 import esb from 'elastic-builder';
 import moment from 'moment';
 import { searchedDataFormator } from '../util/response-formatter';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuid } from 'uuid';
 
 const esClient = ElasticSearchClient.getInstance();
-const sqsClient = SQSClient.getInstance();
 
 const getCoverageRecords = async (
-    RepoIds: string[]
-  ): Promise<Github.Type.TestCoverageResponse[]> => {
-    RepoIds[0]="gh_repo_660498616";
-    const yesterDate = moment().subtract(1, 'days').format('YYYY-MM-DD');
-    const query = esb
-      .requestBodySearch()
-      .size(RepoIds.length)
-      .query(
-        esb.termQuery('body.forDate',yesterDate),
-      )
-      .toJSON();
+  RepoIds: string[]
+): Promise<Github.Type.TestCoverageResponse[]> => {
+  if (RepoIds.length === 0) {
+    throw new Error('RepoIds must be provided.');
+  }
+  const yesterDate = moment().subtract(1, 'days').format('YYYY-MM-DD');
+  const query = esb
+    .requestBodySearch()
+    .size(RepoIds.length)
+    .query(esb.termQuery('body.forDate', yesterDate))
+    .toJSON();
 
-    const data = await esClient.search(Github.Enums.IndexName.GitTestCoverage, query);
-    const records=await searchedDataFormator(data);
-    return records;
-  };
-
+  const data = await esClient.search(Github.Enums.IndexName.GitTestCoverage, query);
+  const records = await searchedDataFormator(data);
+  return records;
+};
 
 export const fetchSaveTestCoverage = async (RepoIds: string[]): Promise<void> => {
-    const dataCoverage = await getCoverageRecords(RepoIds);
-    if (!dataCoverage?.length) {
-      logger.info({
-        message: `fetchSaveTestCoverage.info: GET_GITHUB_BRANCH_DETAILS: No record found for repoIds: ${RepoIds}`
-      });
-      return;
-    }
-    const uniqueID = uuidv4();
-    const currentDate = moment().format('YYYY-MM-DD'); 
-    const updatedDataCoverage = dataCoverage.map(({_id,...hit}) => {
-    const organizationId = hit.organizationId;
-    const repoId=hit.repoId;
-        return {
-          _id:uniqueID,
-            body: {
-              ...hit,
-              id: `${organizationId}_${repoId}_${currentDate}`, 
-              forDate: currentDate
-            }
-        };
+  const dataCoverage = await getCoverageRecords(RepoIds);
+  if (!dataCoverage?.length) {
+    logger.info({
+      message: `fetchSaveTestCoverage.info: GET_GITHUB_BRANCH_DETAILS: No record found for repoIds: ${RepoIds}`,
     });
-    await esClient.bulkInsert(Github.Enums.IndexName.GitTestCoverage,updatedDataCoverage);
-  };
-
-  
+    return;
+  }
+  const uniqueID = uuid();
+  const currentDate = moment().format('YYYY-MM-DD');
+  const updatedDataCoverage = dataCoverage.map(({ _id, ...hit }) => {
+    const organizationId = hit.organizationId;
+    const repoId = hit.repoId;
+    return {
+      _id: uniqueID,
+      body: {
+        ...hit,
+        id: `${organizationId}_${repoId}_${currentDate}`,
+        forDate: currentDate,
+      },
+    };
+  });
+  await esClient.bulkInsert(Github.Enums.IndexName.GitTestCoverage, updatedDataCoverage);
+};
