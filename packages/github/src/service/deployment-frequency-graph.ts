8bb114a7a729a33a95a68a5d1b7720e88643cd33
charchit khandelwal <charchit.khandelwal@studiographene.com>
2024-10-22 17:41:21 +0530

    feat(PX-1908): deployment graph api
    Signed-off-by: charchit khandelwal <charchit.khandelwal@studiographene.com>

---
+++ packages/github/src/service/deployment-frequency-graph.ts
@@ -0,0 +1,37 @@
+import {
+  APIGatewayAuthorizerResult,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from 'aws-lambda';
+import { HttpStatusCode, logger, responseParser } from 'core';
+import { getDeploymentFrequencyGraphData } from 'src/matrics/get-deployment-frequency-graph';
+
+export const handler = async function deploymentFrequencyGraph(
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> {
+  try {
+    const startDate: string = event.queryStringParameters?.startDate || '';
+    const endDate: string = event.queryStringParameters?.endDate || '';
+    const interval: string = event.queryStringParameters?.interval || '';
+    const repoIds: string[] = event.queryStringParameters?.repoIds?.split(',') || [''];
+    const env: string = event.queryStringParameters?.env || '';
+
+    const graphData = await getDeploymentFrequencyGraphData(
+      startDate,
+      endDate,
+      repoIds,
+      interval,
+      env
+    );
+
+    return responseParser
+      .setBody({ graphData })
+      .setMessage('Deployment frequency graph data retrieved successfully')
+      .setStatusCode(HttpStatusCode['200'])
+      .setResponseBodyCode('SUCCESS')
+      .send();
+  } catch (error) {
+    logger.error({ message: 'deploymentFrequencyGraph.error', error: `${error}` });
+    throw new Error(`Something went wrong: ${error}`);
+  }
+};
