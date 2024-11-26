4c40226e6ed2e6675328185f34375ee43c955735
Kaushal965 <kaushal.mittal@studiographene.com>
2024-11-26 13:19:22 +0530

    feat(PX-2020): commit done

--- packages/jira/src/matrics/get-rca-tabular-view.ts
+++ packages/jira/src/matrics/get-rca-tabular-view.ts
@@ -3,6 +3,7 @@ import { Github, Jira } from "abstraction";
 import { IssuesTypes } from "abstraction/jira/enums";
 import { rcaTableRespnose, rcaTableView } from "abstraction/jira/type";
 import esb from "elastic-builder";
+import { head, max } from "lodash";
 
 const esClient = ElasticSearchClient.getInstance();
 
@@ -24,23 +25,31 @@ export async function rcaTableDetailed(
       esb.termQuery(containsField, true)
     )
   )
-    .agg(
+  .agg(
     esb.termsAggregation('rcaCount')
       .field(`body.rcaData.${aggField}`)
       .size(1000)
   );
 
 const esbQuery = query.toJSON();
-const response:rcaTableRespnose = await esClient.queryAggs(Jira.Enums.IndexName.Issue, esbQuery);
-
-const devRcaBuckets = response.rcaCount?.buckets.map((bucket: any) => ({
+const response:any = await esClient.search(Jira.Enums.IndexName.Issue, esbQuery);
+const devRcaBuckets = response.aggregations.rcaCount?.buckets.map((bucket: any) => ({
   name: bucket.key, 
   count: bucket.doc_count, 
 }));
-
   return {
-    headline:'Development RCA Count',
+    headline:getHeadline(response,devRcaBuckets),
     data:devRcaBuckets
   };
 };
-    
+function getHeadline(response:any,buckets:any){
+  let max=0,name="";
+  for(let i=0;i<buckets.length;i++){
+    if(buckets[i].count>max){
+      max=buckets[i].count;
+      name=buckets[i].name;
+    }
+  }
+  const headline = `${name} = ${(max/response.hits.total.value)*100}`
+  return headline
+}
